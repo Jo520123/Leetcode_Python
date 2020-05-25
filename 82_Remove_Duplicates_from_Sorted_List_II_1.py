ed627d9bbed895ebbab3a47510e83d688628c323
Joseph Huan Chih Wang <jo520@unm.edu>
2020-05-25 17:48:28 -0600

    LC_82

---
+++ 82_Remove_Duplicates_from_Sorted_List_II_1.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """"
+            :type head: ListNode
+            :rtype:  ListNode
+        """
+        dummy_head = ListNode(0)
+        dummy_head.next = head
+        pre = dummy_head
+        cur = head
+
+        while cur:
+
+            while cur.next and cur.val == cur.next.val:
+                cur = cur.next
+
+            if pre.next == cur:
+                pre = cur
+
+            else:
+                pre.next = cur.next
+
+            cur = cur.next
+
+        return dummy_head.next
