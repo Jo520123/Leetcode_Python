17ca5f526e9683450cae8fc3fbd7360aef3e070f
Joseph Huan Chih Wang <jo520@unm.edu>
2020-06-23 18:23:25 -0600

    LC

---
+++ 11_ContainerWithMostWater_1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxArea(self, height):
+        l = 0
+        r = len(height)-1
+        result = 0
+
+        while l < r:
+            water = min(height[l], height[r])*(r-l)
+
+            if water > result:
+                result = water
+
+            if height[l] < height[r]:
+                l+=1
+            else:
+                r-=1
+
+        return result
+
